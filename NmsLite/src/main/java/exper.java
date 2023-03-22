e9cbfca84fef03961e04c4f26157014fb495b70d
yasht-007 <official.yasht@gmail.com>
2023-03-22 20:13:28 +0530

    exper remove

--- NmsLite/src/main/java/exper.java
+++
@@ -1,8 +0,0 @@
-public class exper
-{
-    public static void main(String[] args)
-    {
-//        SSHClient sshClient = new SSHClient();
-//        sshClient.poll("harsh","10.20.40.194","Leo102030",22);
-    }
-}
